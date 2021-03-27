e57f13cfda6c9b7d8085a2cf9bbcf01bcdc487d8
Artem Zagarov <artem.zagarov@gmail.com>
2021-03-28 00:46:38 +0200

    added clearTerminal func and destructured code a bit

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 using namespace std;
 
+bool gameOver;
 const int width = 40;
 const int height = 20;
+int x, y, fruitX, fruitY, score;
 
-void draw(){
-    //Drawing the top border
-    for (int i=0; i<width+1; i++) {
+void ClearTerminal(){
+    for (int i = 0; i<100; i++) {
+        cout<< "\n";
+    }
+}
+
+//Function to draw the horizontal line of # symbols
+void DrawLine(int lineLength){
+    for (int i=0; i<lineLength+1; i++) {
         cout << "#";
     }
     cout << endl;
+}
+
+void Setup(){
+    gameOver=false;
+    x = width/2;
+    y = width/2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+ }
+
+void Draw(){
+    //Clearing the terminal
+    ClearTerminal();
+    //Drawing the top border
+    DrawLine(width);
 
     for (int i=0; i<height; i++) {
         for (int j = 0 ; j<width; j++) {
@@ -20,17 +43,16 @@ void draw(){
         }
         cout << endl;
     }
-
     
     //Drawing the bottom border
-    for (int i=0; i<width+1; i++) {
-        cout << "#";
-    }
-    cout << endl;
+    DrawLine(width);
 }
 
 
 int main(int argc, const char * argv[]) {
-    draw();
+    Setup();
+    while (!gameOver) {
+        Draw();
+    }
     return 0;
 }
