2a707236a7b2e2ddb8eb9e9d48d8d87ae69f9b99
Artem Zagarov <artem.zagarov@gmail.com>
2021-03-28 00:05:36 +0200

    init

---
+++ snake/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  snake
+//
+//  Created by Arttu Zagarov on 28.3.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
