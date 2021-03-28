c10a6d0f79f7e62fdefeda920dbc0e0fa2d38f05
Artem Zagarov <artem.zagarov@gmail.com>
2021-03-28 12:40:14 +0300

    changed to ncurse

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,58 +1,81 @@
 #include <iostream>
-using namespace std;
+#include <ncurses.h>
 
 bool gameOver;
 const int width = 40;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
 
-void ClearTerminal(){
-    for (int i = 0; i<100; i++) {
-        cout<< "\n";
-    }
-}
 
 //Function to draw the horizontal line of # symbols
 void DrawLine(int lineLength){
     for (int i=0; i<lineLength+1; i++) {
-        cout << "#";
+        printw("#");
     }
-    cout << endl;
+    printw("\n");
 }
 
 void Setup(){
     gameOver=false;
-    x = width/2;
-    y = width/2;
+    dir = STOP;
+    x = width / 2 - 1;
+    y = height / 2 - 1;
     fruitX = rand() % width;
     fruitY = rand() % height;
- }
+    
+    //Initializing everything for ncurse
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(0);
+}
 
 void Draw(){
-    //Clearing the terminal
-    ClearTerminal();
+    clear();
     //Drawing the top border
     DrawLine(width);
-
+    
     for (int i=0; i<height; i++) {
         for (int j = 0 ; j<width; j++) {
             if (j==0 || j==width-1) {
-                cout << "#";
+                printw("#");
             }
-            cout << " ";
+            if (i == y && j == x) {
+                printw("O");
+            } else if (i == fruitY && j == fruitX ) {
+                printw("F");
+            } else {
+                printw(" ");
+            }
         }
-        cout << endl;
+        printw("\n");
     }
     
     //Drawing the bottom border
     DrawLine(width);
+    
+    printw("Score is: ", score);
+    refresh();
+    getch();
+    endwin();
 }
 
 
 int main(int argc, const char * argv[]) {
     Setup();
-    while (!gameOver) {
-        Draw();
-    }
+    Draw();
+    //    Setup();
+    //    while (!gameOver) {
+    //        Draw();
+    //    }
+//    initscr();
+//    printw("Enter a character: ");
+//    refresh();
+//    getch();
+//    refresh();
+//
     return 0;
 }
